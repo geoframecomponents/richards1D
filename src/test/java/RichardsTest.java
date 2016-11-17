ca7ac84776d9754a7bd9d9274c35acded9e33df4
sidereus <francesco.serafin.3@gmail.com>
2016-11-17 23:03:06 +0100

    Fixed bug in implementing JUnit Tests

--- src/test/java/RichardsTest.java
+++ src/test/java/RichardsTest.java
@@ -1,16 +1,15 @@
+import org.junit.Test;
+
 public class RichardsTest {
- 
-	public static void main(String[] args) {
+
+	@Test
+	public void testTest() {
+
 		RichardsSolver richardssolver = new RichardsSolver();
  
 		Richards richards = richardssolver.solveRichards("1D");
 		
 		System.out.println("My GOD this was hard!");
 
-		@Test
-		public void testTest() 
-		{
-		    return true;
-		}
 	}
 }
