be2f8f8a3e9102291af8b9ca2fc78b3e390731c3
Shappiro <wolfs.bleat@gmail.com>
2016-12-09 21:52:09 +0100

    Added inner iterations

--- src/main/java/richards1d.java
+++ src/main/java/richards1d.java
@@ -38,9 +38,9 @@ public class Richards1d {
 		// Space
 		double 	static 	space_bottom		= 0;
 		double 	static 	space_top			= 2;
-		double 	static 	num_control_volumes	= 100; 
-		double 	static 	space_delta			= (space_top - space_bottom) / num_control_volumes; 			// delta
-		double 	static 	space_cv_centres[]	= seq(space_bottom + space_delta / 2,space_top - space_delta / 2,num_control_volumes); // Centres of the "control volumes"
+		double 	static 	NUM_CONTROL_VOLUMES	= 100; 
+		double 	static 	space_delta			= (space_top - space_bottom) / NUM_CONTROL_VOLUMES; 			// delta
+		double 	static 	space_cv_centres[]	= seq(space_bottom + space_delta / 2,space_top - space_delta / 2,NUM_CONTROL_VOLUMES); // Centres of the "control volumes"
 
 		// Time
 		double 	static 	time_end 			= Math.exp(3,5);            
@@ -53,37 +53,38 @@ public class Richards1d {
 		double 	static	gridvarsq			= Math.pow((time_delta / space_delta),2);		
 
 		// Working variables
-		double[] 		psis 				= new double[num_control_volumes];	
-		double 			psi_in				= 0;								// Initial guess for \psi
-		double[] 		psis_outer			= 0;								// Used for saving \psi of the outer iteration to be plugged inside the inner iteration
+		double[] 		psis 				= new double[NUM_CONTROL_VOLUMES];	
+		double[] 		dpsis 				= new double[NUM_CONTROL_VOLUMES];	
+		double[] 		psis_outer			= new double[NUM_CONTROL_VOLUMES];	// Used for saving \psi of the outer iteration to be plugged inside the inner iteration
 		double 			k_r					= 0;								
 		double 			k_l					= 0;
-		double[] 		kappas				= new double[num_control_volumes];
-		double[] 		a 					= new double[num_control_volumes];
-		double[] 		b 					= new double[num_control_volumes];
-		double[] 		c 					= new double[num_control_volumes];
-		double[] 		fs					= new double[num_control_volumes];
-		double[] 		fk					= new double[num_control_volumes];
-		double[] 		rhss				= new double[num_control_volumes];
+		double[] 		kappas				= new double[NUM_CONTROL_VOLUMES];
+		double[] 		a 					= new double[NUM_CONTROL_VOLUMES];
+		double[] 		b 					= new double[NUM_CONTROL_VOLUMES];
+		double[] 		c 					= new double[NUM_CONTROL_VOLUMES];
+		double[] 		fs					= new double[NUM_CONTROL_VOLUMES];
+		double[] 		fks					= new double[NUM_CONTROL_VOLUMES];
+		double[] 		deltas				= new double[NUM_CONTROL_VOLUMES];
+		double[] 		rhss				= new double[NUM_CONTROL_VOLUMES];
 		double 			k_p					= 0;
 		double 			k_m					= 0;
 		double 			outer_residual		= 0;
 		double 			inner_residual		= 0;
 
 		// Cycle variables
-		int    	static 	max_iterations 		= 100000;
-		int 	static  max_iterations_newt = 100;
+		int    	static 	MAXITER 		= 100000;
+		int 	static  MAXITER_NEWT = 100;
 		int 	static	newton_tolerance	= Math.pow(10,-12)
 
 		// Initial domain conditions
-		for(int i = 0; i <= num_control_volumes; i++) {
+		for(int i = 0; i <= NUM_CONTROL_VOLUMES; i++) {
 			psi[i] = -y[i];
 		}
 
 		////////////////////
 		//// MAIN CYCLE ////
 		////////////////////
-		for(int niter = 0; niter < max_iterations; niter++) {
+		for(int niter = 0; niter < MAXITER; niter++) {
 		    if(time_initial + time_delta > time_end) {
 		        time_delta = time_end-time_initial;
 		    }
@@ -102,13 +103,13 @@ public class Richards1d {
 			//// KAPPAS ////
 		    k_r = kappa(psi_r); 
 		    k_l = kappa(psi_l);
-		    for(int i = 0; i < max_iterations; i++) {
+		    for(int i = 0; i < MAXITER; i++) {
 			    theta[i] = thetaf(theta_s, theta_r, alpha, psi, n, m, psis[i]);
 			    kappas[i] = kappa(alpha, theta_2, theta_r, psi[i]);           
 			}
 
 			//// RIGHT HAND SIDE ////
-			for(int i = 0; i < max_iterations; i++) {
+			for(int i = 0; i < MAXITER; i++) {
 				if( i==1 ) {
 
 		            k_p = 0.5*(kappas[i] + kappas[i+1]);
@@ -118,7 +119,7 @@ public class Richards1d {
 		            c[i] = -k_p * gridvarsq;
 		            rhs[i] = thetas[i] + gridvar * (k_p-k_m) + 2 * k_m * gridvarsq * psi_l; 
 
-				} else if(i == max_iterations) {
+				} else if(i == MAXITER) {
 
 		            k_p = 0.5*(kappas[i] + k_r);
 		            k_m = 0.5*(kappas[i] + k_l);
@@ -140,22 +141,22 @@ public class Richards1d {
 			}
 
 			// Initial guess of psis
-			for(int i = 0; i < num_control_volumes; i++) {
+			for(int i = 0; i < NUM_CONTROL_VOLUMES; i++) {
 				psis[i] = Math.min(psis[i], psic);
 			}
 
 			//// NESTED NEWTON ////
 		    //// OUTER CYCLE, linearizes one of q_{1}, q_{2} ////
-		    for(int i = 0; i < max_iterations_newt; i++) {
+		    for(int i = 0; i < MAXITER_NEWT; i++) {
 
-		    	for(int ii=0; ii < max_iterations; ii++) {
+		    	for(int ii=0; ii < MAXITER; ii++) {
 
 		    		fs[i] = thetaf(psi[i]) - rhs[i];        
 
 			        if(i == 1) {
 			            f[i] = f[i] + b[i]*psi[i] + c[i]*psi[i+1];
 			        }
-			        else if(i == max_iterations) {
+			        else if(i == MAXITER) {
 			            f[i] = f[i] + a[i]*psi[i-1] + b[i]*psi[i];
 			        }
 			        else {
@@ -172,33 +173,39 @@ public class Richards1d {
     			System.arraycopy(psis, 0, psis_outer, 0);
 
 				// Initial guess of psis
-				for(int ii = 0; ii < num_control_volumes; ii++) {
+				for(int ii = 0; ii < NUM_CONTROL_VOLUMES; ii++) {
 					psis[ii] = Math.max(psis[ii], psic);
 				}
 
 			    //// INNER CYCLE ////
-				for(int j = 0; j < num_control_volumes; j++) {
+				for(int j = 0; j < NUM_CONTROL_VOLUMES; j++) {
 
-					for(int l=0; l < max_iterations; l++) {
+					for(int l=0; l < MAXITER; l++) {
 
-				        fk(i)=Theta1(psi(i))-(Theta2(psik(i))+dTheta2(psik(i))*(psi(i)-psik(i)))-rhs(i);  %Tk is frozen
-				            if(i==1)
-				                fk(i)=fk(i)+b(i)*psi(i)+c(i)*psi(i+1);
-				            elseif(i==IMAX)
-				                fk(i)=fk(i)+a(i)*psi(i-1)+b(i)*psi(i);
-				            else
-				                fk(i)=fk(i)+a(i)*psi(i-1)+b(i)*psi(i)+c(i)*psi(i+1);
-				            end
-				            di(i)=dTheta1(psi(i))-dTheta2(psik(i));
-				        end
-				        inres=sqrt(sum(fk.*fk));
-			    		
+				        fks(l)=theta1(psis[l]) - (theta2(psis_outer[l])+dtheta2(psis_outer[l])*(psis[l] - psis_outer[l])) - rhs[l];
+			            if(l == 1) {
+			                fks[i] = fks[i] + b[l]*psis[l] + c[l]*psis[l+1];
+			            }
+			            else if(l == MAXITER) {
+			                fks[l]=fks[l] + a[l]*psi[l-1] + b[l]*psis[l];
+			            }
+			            else {
+			                fks[l]=fks[i] + a[l]*psis[l-1] + b[i]*psis[l] + c[l]*psis[l+1];
+			            }
+			            deltas[l] = dtheta1(psis[l]) - dtheta2(psis_outer[l]);
+				        inner_residual += fks[l];
+
 			    		System.out.println('Inner iteration ' + l + ' with residual ' +  inner_residual));    
-				        if(inres<tol) {
+				        if(inner_residual < newton_tolerance) {
 				            break;
 				        }
-				        dpsi = Thomas(a,b+di,c,fk); %inner Newton step
-				        psi = psi(:)-dpsi(:); %update temperature at the inner iteration
+					    // TODO (MUST RETURN AN ARRAY)
+					    //// CONJGRAD ////
+				        dpsis = Thomas(a,b+deltas,c,fks);
+					    //// PSIS UPDATE ////
+				        for(int s = 0; s < NUM_CONTROL_VOLUMES; s++) {
+				        	psis[s] = psis[s] - dpsis[s];
+				        }
 
 					}
 
@@ -213,61 +220,71 @@ public class Richards1d {
 
 
 
+		////////////////////
+		//// METHODS ////
+		////////////////////
 
-	/**
-	 * Returns a vector (array of doubles) of equally spaced numbers between a given 
-	 * lower and upper bound
-	 * <p>
-	 * The lower and upper bounds need not be in growing order: in 
-	 * case min>max, the method generates a sequence of decreasing 
-	 * real numbers
-	 *
-	 * @param  max  	a real number, the value of the lower bound of the sequence
-	 * @param  max  	a real number, the value of the upper bound of the sequence
-	 * @param  points  	an integer, the number of equally spaced points in the sequence 
-	 * @return 			a vector of equally spaced real numbers
-	 */	
-	public static double[] seq(double min, double max, int points) {  
-	    double[] sequence = new double[points];  
-	    for (int i = 0; i < points; i++){  
-	        sequence[i] = min + i * (max - min) / (points - 1);  
-	    }  
-	    return sequence;  
-	}
-
-	/**
-	 * Returns a K given the parameters
-	 * <p>
-	 * 
-	 *
-	 *
-	 *
-	 * @param  alpha  	a real number, the value of the lower bound of the sequence
-	 * @param  theta_s  an integer, the number of equally spaced points in the sequence 
-	 * @param  theta_r  an integer, the number of equally spaced points in the sequence 
-	 * @return 			a real number storing the control volume's K
-	 */	
-	public static double k(double alpha, double theta_s, double theta_r, double psi) {
-
-		double kappa;
-		double saturation;
-
-		saturation = (thetaf(psi) - theta_r) / (theta_s - theta_r); 
-		kappa = Ks * sqrt(saturation) * Math.pow(1 - (1 - Math.pow(Math.pow(sat,1/m)),m), 2);
-		return kappa;
-	}
-
-	// Computes the \theta_{f}
-	public static double thetaf(double theta_s, double theta_r, double alpha, double psi, double n, double m) {
-
-		double theta_f;
-
-		if(psi <= 0) {
-		    theta_f = theta_r + (theta_s-theta_r)/( (1+Math.abs(alpha*psi)^n)^m );
-		} else {
-		    theta_f = theta_s;
+		/**
+		 * Returns a vector (array of doubles) of equally spaced numbers between a given 
+		 * lower and upper bound
+		 * <p>
+		 * The lower and upper bounds need not be in growing order: in 
+		 * case min>max, the method generates a sequence of decreasing 
+		 * real numbers
+		 *
+		 * @param  max  	a real number, the value of the lower bound of the sequence
+		 * @param  max  	a real number, the value of the upper bound of the sequence
+		 * @param  points  	an integer, the number of equally spaced points in the sequence 
+		 * @return 			a vector of equally spaced real numbers
+		 */	
+		public static double[] seq(double min, double max, int points) {  
+		    double[] sequence = new double[points];  
+		    for (int i = 0; i < points; i++){  
+		        sequence[i] = min + i * (max - min) / (points - 1);  
+		    }  
+		    return sequence;  
 		}
-		return theta_f;
-	}
+
+		/**
+		 * Returns a K, given the soil parameters
+		 *
+		 * @param  alpha  	
+		 * @param  theta_s   
+		 * @param  theta_r   
+		 * @param  psi  	 
+		 * @param  m   
+		 * @return 			a real number representing the control volume's K
+		 */	
+		public static double k(double theta_s, double theta_r, double psi, double m) {
+
+			double kappa;
+			double saturation;
+
+			saturation = (thetaf(psi) - theta_r) / (theta_s - theta_r); 
+			kappa = Ks * sqrt(saturation) * Math.pow(1 - (1 - Math.pow(Math.pow(sat,1/m)),m), 2);
+			return kappa;
+		}
+
+		/**
+		 * Returns the thetaf term
+		 *
+		 * @param  alpha  	
+		 * @param  theta_s   
+		 * @param  theta_r   
+		 * @param  psi  	 
+		 * @param  m   
+		 * @return 			a real number representing the \theta_{f}
+		 */	
+		public static double thetaf(double theta_s, double theta_r, double alpha, double psi, double n, double m) {
+
+			double theta_f;
+
+			if(psi <= 0) {
+			    theta_f = theta_r + (theta_s-theta_r)/( (1+Math.abs(alpha*psi)^n)^m );
+			} else {
+			    theta_f = theta_s;
+			}
+			return theta_f;
+		}
 
 }
