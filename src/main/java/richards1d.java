d9c30e7068bd6e37b03b4de9db375f1c72cde0b3
cifciaf <cifciaf@cifciaf-laptop>
2016-12-09 16:28:05 +0100

    File position cleaning

--- src/main/java/richards1d.java
+++ src/main/java/richards1d.java
@@ -1,3 +1,22 @@
+/*
+ * GNU GPL v3 License
+ *
+ * Copyright 2015 Marialaura Bancheri
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.lang.Math
 
 public class Richards1d {
@@ -64,7 +83,7 @@ public class Richards1d {
 		
 		   	time = time + time_delta;
 		   	if(time > time_end) {
-		   		
+
 		   	}
 		}
 
