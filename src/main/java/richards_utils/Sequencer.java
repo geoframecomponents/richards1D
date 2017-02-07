b2f7f2a84f84c1b472d1b37f9852e83fc579df28
Shappiro <wolfs.bleat@gmail.com>
2017-02-07 14:46:07 +0100

    Dropped static attribute for seq method

--- src/main/java/richards_utils/Sequencer.java
+++ src/main/java/richards_utils/Sequencer.java
@@ -8,7 +8,7 @@ public class Sequencer {
 	 * @param points, number of values in the sequence
 	 * @return sequence[] values in the sequence
 	 */
-	public static double[] seq(double min, double max, int points) {  
+	public double[] seq(double min, double max, int points) {  
 	    double[] sequence = new double[points];  
 	    for (int i = 0; i < points; i++){  
 	        sequence[i] = min + i * (max - min) / (points - 1);  
