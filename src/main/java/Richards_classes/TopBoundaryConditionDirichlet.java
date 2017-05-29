0333967e2afd0d5127d5516005aef9912811832f
niccolotubini <niccolo.tubini@studenti.unitn.it>
2017-05-29 13:58:38 +0200

    Modified the parameters of the class in order to properly account of the right mesh space

--- src/main/java/Richards_classes/TopBoundaryConditionDirichlet.java
+++ src/main/java/Richards_classes/TopBoundaryConditionDirichlet.java
@@ -2,59 +2,54 @@ package richards_classes;
 
 public class TopBoundaryConditionDirichlet extends BoundaryCondition{
 
-	public double upperDiagonal(double bC, double kP, double kM, double gridvarsqP, double gridvarsqM, double gridvarP, double gridvarM) {
+	public double upperDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep) {
 		this.bC = bC;
 		this.kP = kP;
 		this.kM = kM;
-		this.gridvarP = gridvarP;
-		this.gridvarM = gridvarM;
-		this.gridvarsqP = gridvarsqP;
-		this.gridvarsqM = gridvarsqM;
+		this.spaceDeltaP = spaceDeltaP;
+		this.spaceDeltaM = spaceDeltaM;
+		this.tTimestep = tTimestep;
 		this.term = 0;
 
 		return term;
 	}
 	
-	public double mainDiagonal(double bC, double kP, double kM, double gridvarsqP, double gridvarsqM, double gridvarP, double gridvarM) {
+	public double mainDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep) {
 		this.bC = bC;
 		this.kP = kP;
 		this.kM = kM;
-		this.gridvarP = gridvarP;
-		this.gridvarM = gridvarM;
-		this.gridvarsqP = gridvarsqP;
-		this.gridvarsqM = gridvarsqM;
-		
-		this.term = this.gridvarsqM * this.kM + this.gridvarsqP*2*this.kP;
+		this.spaceDeltaP = spaceDeltaP;
+		this.spaceDeltaM = spaceDeltaM;
+		this.tTimestep = tTimestep;
 
+		this.term = this.tTimestep/(this.spaceDeltaM/2+this.spaceDeltaP)*1/this.spaceDeltaM*this.kM + this.tTimestep/(this.spaceDeltaM/2+this.spaceDeltaP)*1/this.spaceDeltaP*this.kP; 
 		return term;
 
 	}
 	
-	public double lowerDiagonal(double bC, double kP, double kM, double gridvarsqP, double gridvarsqM, double gridvarP, double gridvarM) {
+	public double lowerDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep) {
 		this.bC = bC;
 		this.kP = kP;
 		this.kM = kM;
-		this.gridvarP = gridvarP;
-		this.gridvarM = gridvarM;
-		this.gridvarsqP = gridvarsqP;
-		this.gridvarsqM = gridvarsqM;
+		this.spaceDeltaP = spaceDeltaP;
+		this.spaceDeltaM = spaceDeltaM;
+		this.tTimestep = tTimestep;
 		
-		this.term = -this.kM * this.gridvarsqM;
+		this.term = -this.tTimestep/(this.spaceDeltaM/2+this.spaceDeltaP)*1/this.spaceDeltaM*this.kM;
 
 		return term;
 
 	}
 
-	public double rightHandSide(double bC, double kP, double kM, double gridvarsqP, double gridvarsqM, double gridvarP, double gridvarM) {
+	public double rightHandSide(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep) {
 		this.bC = bC;
 		this.kP = kP;
 		this.kM = kM;
-		this.gridvarP = gridvarP;
-		this.gridvarM = gridvarM;
-		this.gridvarsqP = gridvarsqP;
-		this.gridvarsqM = gridvarsqM;
+		this.spaceDeltaP = spaceDeltaP;
+		this.spaceDeltaM = spaceDeltaM;
+		this.tTimestep = tTimestep;
 		
-		this.term = this.gridvarP * this.kP - this.gridvarM*this.kM + 2 * this.kP * this.gridvarsqP * this.bC;
+		this.term = this.tTimestep/(this.spaceDeltaM/2+this.spaceDeltaP) * this.kP - this.tTimestep/(this.spaceDeltaM/2+this.spaceDeltaP)*this.kM + this.kP * this.tTimestep/(this.spaceDeltaM/2+this.spaceDeltaP)*1/this.spaceDeltaP * this.bC;
 
 		return term;
 
