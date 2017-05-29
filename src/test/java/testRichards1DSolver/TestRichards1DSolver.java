9d5d72cfa076dd963f1e993bdb2597f56c67995a
niccolotubini <niccolo.tubini@studenti.unitn.it>
2017-05-29 14:03:35 +0200

    The reader for the initial condition now receives a .csv input  file

--- src/test/java/testRichards1DSolver/TestRichards1DSolver.java
+++ src/test/java/testRichards1DSolver/TestRichards1DSolver.java
@@ -41,15 +41,15 @@ public class TestRichards1DSolver {
 	public void Test() throws Exception {
 
 
-		String startDate = "1991-09-18 00:00" ;
-		String endDate = "1992-09-18 00:00";
-		int timeStepMinutes = 60*24;
+		String startDate = "2017-02-25 00:00" ;
+		String endDate = "2017-05-20 00:00";
+		int timeStepMinutes = 5;
 		String fId = "ID";
 
 
-		String pathTopBC ="resources/Input/D_TopBoundaryConditionREAL.csv";
-		String pathBottomBC ="resources/Input/D_BottomBoundaryCondition.csv";
-		String pathIC = "resources/Input/InitialConditionTEST.txt";
+		String pathTopBC ="resources/Input/Trento.csv";//"resources/Input/D_TopBoundaryConditionREAL.csv";
+		String pathBottomBC ="resources/Input/TrentoBottom.csv";
+		String pathIC = "resources/Input/InitialConditionHydrostatic.csv";
 
 		OmsTimeSeriesIteratorReader topBCReader = getTimeseriesReader(pathTopBC, fId, startDate, endDate, timeStepMinutes);
 		OmsTimeSeriesIteratorReader bottomBCReader = getTimeseriesReader(pathBottomBC, fId, startDate, endDate, timeStepMinutes);
@@ -59,24 +59,25 @@ public class TestRichards1DSolver {
 
 		Richards1DSolver R1DSolver = new Richards1DSolver();
 
-		R1DSolver.ks = 0.0000743;
-		R1DSolver.thetaS =0.375 ;
-		R1DSolver.thetaR = 0.058;
-		R1DSolver.n = 3.177;
-		R1DSolver.alpha =3.52 ;
+		R1DSolver.ks = 1.5167/10000;//0.062/(3600*24);6.26/1000;//
+		R1DSolver.thetaS =0.4686;//0.41;0.3685;//
+		R1DSolver.thetaR = 0.1060;//0.095;0.0286;//
+		R1DSolver.n = 1.3954;//1.312.2390;//
+		R1DSolver.alpha = 0.0104/0.01;//1.9;0.0280/0.01;//
 		R1DSolver.lambda =1.9 ;
 		R1DSolver.psiE = 1.9;
 		R1DSolver.rMedian =1.9 ;
 		R1DSolver.sigma =1.9 ;
 		R1DSolver.soilHydraulicModel = "VanGenuchten";
-		R1DSolver.topBCType = "Top Dirichlet";
-		R1DSolver.bottomBCType = "Bottom Free Drainage";
+		R1DSolver.topBCType = "Top Neumann";
+		R1DSolver.bottomBCType = "Bottom Dirichlet";
 		R1DSolver.spaceBottom = 2.0;
 		R1DSolver.tTimestep = timeStepMinutes*60;
-		R1DSolver.newtonTolerance = Math.pow(10,-3);
+		R1DSolver.newtonTolerance = Math.pow(10,-6);
 		R1DSolver.iC = iC;
 		R1DSolver.depth = depth;
 		R1DSolver.dir = "resources/Output";
+		R1DSolver.nestedNewton = 0;
 		while( topBCReader.doProcess  ) {
 
 			topBCReader.nextRecord();	
@@ -139,21 +140,23 @@ public class TestRichards1DSolver {
 	}
 	*/
 	private double[] ReadAndStoreIC(String filePath) throws IOException{
+		
 		List<Double> l1 = new ArrayList<>();
 		List<Double> l2 = new ArrayList<>();
 		double[] readVector;
 		
-		Scanner s = new Scanner(new FileReader(filePath));
-
-		while (s.hasNext()) {
-		    l1.add(s.nextDouble());
-		    l2.add(s.nextDouble());
+		BufferedReader fileReader = new BufferedReader(new FileReader(filePath));
+		String eachLine = "";
+		while ((eachLine = fileReader.readLine()) != null) {
+            String[] values = eachLine.split(",");
+            l1.add(Double.parseDouble(values[0]));
+		    l2.add(Double.parseDouble(values[1]));
 		}
 
-		s.close();
-		
+		fileReader.close();
+        
 		readVector = new double[l1.size()];
-		
+					
 		for(int i = 0;i < l1.size();i++){
 			readVector[i] = l2.get(i);
 		}
@@ -161,25 +164,28 @@ public class TestRichards1DSolver {
 
 	}
 	private double[] ReadAndStoreDepth(String filePath) throws IOException{
+		
 		List<Double> l1 = new ArrayList<>();
 		List<Double> l2 = new ArrayList<>();
 		double[] readVector;
 		
-		Scanner s = new Scanner(new FileReader(filePath));
-
-		while (s.hasNext()) {
-		    l1.add(s.nextDouble());
-		    l2.add(s.nextDouble());
+		BufferedReader fileReader = new BufferedReader(new FileReader(filePath));
+		String eachLine = "";
+		while ((eachLine = fileReader.readLine()) != null) {
+            String[] values = eachLine.split(",");
+            l1.add(Double.parseDouble(values[0]));
+		    l2.add(Double.parseDouble(values[1]));
 		}
 
-		s.close();
-		
+		fileReader.close();
+        
 		readVector = new double[l1.size()];
-		
+					
 		for(int i = 0;i < l1.size();i++){
 			readVector[i] = l1.get(i);
 		}
 		return readVector;
+
 
 	}
 	
