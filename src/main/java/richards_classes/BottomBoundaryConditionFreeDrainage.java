a49fcfe395f55140175c1fa2fe43ca8bcd8da4e7
niccolotubini <niccolo.tubini@studenti.unitn.it>
2017-06-20 15:49:16 +0200

    This solver allows to deal with a tilted ground surface

--- src/main/java/richards_classes/BottomBoundaryConditionFreeDrainage.java
+++ src/main/java/richards_classes/BottomBoundaryConditionFreeDrainage.java
@@ -2,40 +2,43 @@ package richards_classes;
 
 public class BottomBoundaryConditionFreeDrainage extends BoundaryCondition {
 
-	public double upperDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep) {
+	public double upperDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep, double delta) {
 		this.bC = bC;
 		this.kP = kP;
 		this.kM = kM;
 		this.spaceDeltaP = spaceDeltaP;
 		this.spaceDeltaM = spaceDeltaM;
 		this.tTimestep = tTimestep;
+		this.delta = delta;
 		
-		this.term = -this.kP * this.tTimestep/(this.spaceDeltaM+this.spaceDeltaP/2)*1/this.spaceDeltaP;
+		this.term = -this.kP * this.tTimestep/(this.spaceDeltaM+this.spaceDeltaP/2)*1/this.spaceDeltaP*1/Math.pow(2, Math.cos(this.delta));
 		
 		return term;
 	}
 	
-	public double mainDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep) {
+	public double mainDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep, double delta) {
 		this.bC = bC;
 		this.kP = kP;
 		this.kM = kM;
 		this.spaceDeltaP = spaceDeltaP;
 		this.spaceDeltaM = spaceDeltaM;
 		this.tTimestep = tTimestep;
+		this.delta = delta;
 		
-		this.term = this.tTimestep/(this.spaceDeltaM+this.spaceDeltaP/2)*1/this.spaceDeltaP * this.kP ;
+		this.term = this.tTimestep/(this.spaceDeltaM+this.spaceDeltaP/2)*1/this.spaceDeltaP*1/Math.pow(2, Math.cos(this.delta))* this.kP ;
 		
 		return term;
 
 	}
 	
-	public double lowerDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep) {
+	public double lowerDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep, double delta) {
 		this.bC = bC;
 		this.kP = kP;
 		this.kM = kM;
 		this.spaceDeltaP = spaceDeltaP;
 		this.spaceDeltaM = spaceDeltaM;
 		this.tTimestep = tTimestep;
+		this.delta = delta;
 		
 		this.term = 0;
 
@@ -43,15 +46,17 @@ public class BottomBoundaryConditionFreeDrainage extends BoundaryCondition {
 
 	}
 
-	public double rightHandSide(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep) {
+	public double rightHandSide(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep, double delta) {
 		this.bC = bC;
 		this.kP = kP;
 		this.kM = kM;
 		this.spaceDeltaP = spaceDeltaP;
 		this.spaceDeltaM = spaceDeltaM;
 		this.tTimestep = tTimestep;
+		this.delta = delta;
 		
 		this.term = -this.kM*this.tTimestep/(this.spaceDeltaM+this.spaceDeltaP/2) + this.kP*this.tTimestep/(this.spaceDeltaM+this.spaceDeltaP/2); 
+		
 		return term;
 
 	}	
