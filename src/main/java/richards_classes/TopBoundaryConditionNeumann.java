744b6108848521e2f42ec071b5e7094c171777e3
niccolotubini <niccolo.tubini@studenti.unitn.it>
2017-06-20 15:50:47 +0200

    This solver allows to deal with a tilted ground surface

--- src/main/java/richards_classes/TopBoundaryConditionNeumann.java
+++ src/main/java/richards_classes/TopBoundaryConditionNeumann.java
@@ -2,56 +2,60 @@ package richards_classes;
 
 public class TopBoundaryConditionNeumann extends BoundaryCondition{
 
-	public double upperDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep) {
+	public double upperDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep, double delta) {
 		this.bC = bC;
 		this.kP = kP;
 		this.kM = kM;
 		this.spaceDeltaP = spaceDeltaP;
 		this.spaceDeltaM = spaceDeltaM;
 		this.tTimestep = tTimestep;
+		this.delta = delta;
 		
 		this.term = 0;
 
 		return term;
 	}
 	
-	public double mainDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep) {
+	public double mainDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep, double delta) {
 		this.bC = bC;
 		this.kP = kP;
 		this.kM = kM;
 		this.spaceDeltaP = spaceDeltaP;
 		this.spaceDeltaM = spaceDeltaM;
 		this.tTimestep = tTimestep;
+		this.delta = delta;
 		
-		this.term = this.tTimestep/(this.spaceDeltaM/2+this.spaceDeltaP)*1/this.spaceDeltaM* (this.kM);
+		this.term = this.tTimestep/(this.spaceDeltaM/2+this.spaceDeltaP)*1/this.spaceDeltaM*1/Math.pow(2, Math.cos(this.delta))*this.kM;
 
 		return term;
 
 	}
 	
-	public double lowerDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep) {
+	public double lowerDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep, double delta) {
 		this.bC = bC;
 		this.kP = kP;
 		this.kM = kM;
 		this.spaceDeltaP = spaceDeltaP;
 		this.spaceDeltaM = spaceDeltaM;
 		this.tTimestep = tTimestep;
+		this.delta = delta;
 		
-		this.term = -this.kM * this.tTimestep/(this.spaceDeltaM/2+this.spaceDeltaP)*1/this.spaceDeltaM;
+		this.term = -this.kM * this.tTimestep/(this.spaceDeltaM/2+this.spaceDeltaP)*1/this.spaceDeltaM*1/Math.pow(2, Math.cos(this.delta));
 
 		return term;
 
 	}
 
-	public double rightHandSide(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep) {
+	public double rightHandSide(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep, double delta) {
 		this.bC = bC;
 		this.kP = kP;
 		this.kM = kM;
 		this.spaceDeltaP = spaceDeltaP;
 		this.spaceDeltaM = spaceDeltaM;
 		this.tTimestep = tTimestep;
+		this.delta = delta;
 		
-		this.term = this.tTimestep/(this.spaceDeltaM/2+this.spaceDeltaP) * this.bC - this.tTimestep/(this.spaceDeltaM/2+this.spaceDeltaP)*this.kM;
+		this.term = this.tTimestep/(this.spaceDeltaM/2+this.spaceDeltaP)*this.bC - this.tTimestep/(this.spaceDeltaM/2+this.spaceDeltaP)*this.kM;
 
 		return term;
 
