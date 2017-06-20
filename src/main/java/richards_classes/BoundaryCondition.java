ea1abb98239eab7f9a2f17178aaf96978fb23f55
niccolotubini <niccolo.tubini@studenti.unitn.it>
2017-06-20 15:49:45 +0200

    This solver allows to deal with a tilted ground surface

--- src/main/java/richards_classes/BoundaryCondition.java
+++ src/main/java/richards_classes/BoundaryCondition.java
@@ -9,13 +9,14 @@ public abstract class BoundaryCondition {
 	protected double spaceDeltaP;
 	protected double spaceDeltaM;
 	protected double tTimestep;
+	protected double delta;
 	protected double term;
 	
-	public abstract double upperDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep);
+	public abstract double upperDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep, double delta);
 	
-	public abstract double mainDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep);
+	public abstract double mainDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep, double delta);
 	
-	public abstract double lowerDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep);
+	public abstract double lowerDiagonal(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep, double delta);
 	
-	public abstract double rightHandSide(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep);
+	public abstract double rightHandSide(double bC, double kP, double kM, double spaceDeltaP, double spaceDeltaM, double tTimestep, double delta);
 }
