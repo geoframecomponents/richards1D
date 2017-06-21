b98f9f1efbde47945c2c68f9bfccd56e3310dda8
niccolotubini <niccolo.tubini@studenti.unitn.it>
2017-06-21 19:03:44 +0200

    This class is used to read the initial condition file. This file has to be a csv file with two columns: the first one contains the depth of the centers of the control volumes, the second one the suction values in these points.

---
+++ src/main/java/richards_classes/ReadCsvTwoColumns.java
@@ -0,0 +1,45 @@
+package richards_classes;
+
+import oms3.annotations.*;
+import java.io.*;
+import java.util.*;
+
+public class ReadCsvTwoColumns {
+	@Description("Path of the input file")
+	@In
+	public String filePath;
+	
+	@Description("Depth")
+	@Out
+	public double[] depth;
+	@Description("Suction")
+	@Out
+	public double[] suctionIC;
+	
+	@Execute
+	public void process() throws IOException {
+		
+		List<Double> l1 = new ArrayList<>();
+		List<Double> l2 = new ArrayList<>();
+		
+		BufferedReader fileReader = new BufferedReader(new FileReader(filePath));
+		String eachLine = "";
+		while ((eachLine = fileReader.readLine()) != null) {
+            String[] values = eachLine.split(",");
+            l1.add(Double.parseDouble(values[0]));
+		    l2.add(Double.parseDouble(values[1]));
+		}
+
+		fileReader.close();
+        
+		depth = new double[l1.size()];
+		suctionIC = new double[l2.size()];
+		
+		for(int i = 0;i < l1.size();i++){
+			depth[i] = l1.get(i);
+			suctionIC[i] = l2.get(i);
+		}
+
+	}
+}
+
