0203ef61d694e13625dfcea9187f9aebfbdf57bc
niccolotubini <tubini.niccolo@gmail.com>
2018-03-28 15:40:33 +0200

    Package description

---
+++ src/main/java/thermalParameterization/package-info.java
@@ -0,0 +1,32 @@
+/*
+ * GNU GPL v3 License Niccolo' Tubini and Riccardo Rigon
+ *
+ * Copyright 2017 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This package collects all the parameterizations needed to integrate the energy equation in a porous media. 
+ * These parameterizations concerns the thermal parameterizations.
+ * Thermal properties: 
+ * 		- thermal conductivity;
+ * 		- thermal capacity.
+ * 
+ */
+/**
+ * @author Niccolo' Tubini
+ *
+ */
+package thermalParameterization;
