c71ac734e2ca0f882582cccac6aa3dffe3b90234
niccolotubini <tubini.niccolo@gmail.com>
2018-05-03 09:37:04 +0200

    Added a new set method to deal with soil heterogeneity. Moreover some commented line that are a draft of what I have to modify to complete the task to deal with soil heterogeneity.

--- src/main/java/richards_classes/NestedNewton.java
+++ src/main/java/richards_classes/NestedNewton.java
@@ -115,6 +115,10 @@ public class NestedNewton {
 	 * @param mainDiagonal main diagonal of the coefficient matrix A of the linear system, it is a vector of length NUM_CONTROL_VOLUMES
 	 * @param lowerDiagonal lower diagonal of the coefficient matrix A of the linear system, it is a vector of length NUM_CONTROL_VOLUMES
 	 * @param rhss right hand side term of the linear system, it is a vector of length NUM_CONTROL_VOLUMES
+	 * @param thetaS vector containing the adimensional water content at saturation for each control volume, it is a vector of length NUM_CONTROL_VOLUMES-1
+	 * @param thetaR vector containing the adimensional residual water contentfor each control volume, it is a vector of length NUM_CONTROL_VOLUMES-1
+	 * @param par1SWRC vector containing the first parameter of the SWRC, it is a vector of length NUM_CONTROL_VOLUMES-1
+	 * @param par2SWRC vector containing the second parameter of the SWRC, it is a vector of length NUM_CONTROL_VOLUMES-1
 	 */
 	public void set(double[] psis, double[] mainDiagonal, double[] upperDiagonal, double[] lowerDiagonal, double[] rhss, double[] thetaS, double[] thetaR, double[] par1SWRC, double[] par2SWRC){
 		this.psis = psis;
@@ -215,14 +219,7 @@ public class NestedNewton {
 							dis[l] = totalDepthJordanDecomposition.p(psis[l]) - totalDepthJordanDecomposition.q(psis_outer[l]);
 							//System.out.println(l+" "+fks[l]);
 							//System.out.println(l+" "+totalDepthJordanDecomposition.p(psis[l]));
-						} //else if(l==NUM_CONTROL_VOLUMES-2) {
-							// //soilMoistureJordanDecomposition.setSoilParametrization(thetaS[i], thetaR[i], par1SWRC[i], par2SWRC[i]);
-							//fks[l] = soilMoistureJordanDecomposition.pIntegral(psis[l])*dx[l] - ( soilMoistureJordanDecomposition.qIntegral(psis_outer[l]) + soilMoistureJordanDecomposition.q(psis_outer[l])*(psis[l] - psis_outer[l]) )*dx[l] - this.rhss[l]  + lowerDiagonal[l]*psis[l-1] + mainDiagonal[l]*psis[l] + upperDiagonal[l]*psis[l+1];
-							//dis[l] = ( soilMoistureJordanDecomposition.p(psis[l]) - soilMoistureJordanDecomposition.q(psis_outer[l]) )*dx[l];
-							//System.out.println(l+" "+fks[l]);
-							//System.out.println(l+" "+dis[l]);
-						//} 
-						else {
+						} else {
 							//soilMoistureJordanDecomposition.setSoilParametrization(thetaS[i], thetaR[i], par1SWRC[i], par2SWRC[i]);
 							fks[l] = soilMoistureJordanDecomposition.pIntegral(psis[l])*dx[l] - ( soilMoistureJordanDecomposition.qIntegral(psis_outer[l]) + soilMoistureJordanDecomposition.q(psis_outer[l])*(psis[l] - psis_outer[l]) )*dx[l] - this.rhss[l]  + lowerDiagonal[l]*psis[l-1] + mainDiagonal[l]*psis[l] + upperDiagonal[l]*psis[l+1];
 							dis[l] = ( soilMoistureJordanDecomposition.p(psis[l]) - soilMoistureJordanDecomposition.q(psis_outer[l]) )*dx[l];
